c98a2f935201e0581f5a2710da9e15e3f77c7f3c
rokugensai <rokugensai@gmail.com>
2007-12-18 09:57:06 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/fisshplate/trunk@33 c159f9c0-7f7c-4dca-964d-717b2124a8cb

---
+++ s2fisshplate/src/test/java/org/seasar/fisshplate/interceptor/S2FisshplateInterceptorTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.fisshplate.interceptor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.seasar.extension.unit.S2TestCase;
+
+/**
+ * @author rokugen
+ */
+public class S2FisshplateInterceptorTest extends S2TestCase {
+	private TestFisshplate fisshplate;
+	
+	protected void setUp()throws Exception{
+		include("test.dicon");		
+	}
+	
+	protected void tearDown(){
+		
+	}
+	
+	public void testインタセプタのテスト(){
+		TestFisshplateDto dto =new TestFisshplateDto();
+		dto.setTitle("タイトルです");
+		List<TestItem> itemList = new ArrayList<TestItem>();
+		itemList.add(new TestItem("1行目",10,new Date()));
+		itemList.add(new TestItem("2行目",20,new Date()));
+		itemList.add(new TestItem("3行目",30,new Date()));
+		
+		dto.setItemList(itemList);
+		
+		HSSFWorkbook wb = fisshplate.getTestWb(dto);
+		
+	}
+
+}
