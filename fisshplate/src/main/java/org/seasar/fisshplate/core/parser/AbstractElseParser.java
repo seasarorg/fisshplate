2ff18905869a7d0f63ca17f2a3701f3a93f08308
rokugensai <rokugensai@gmail.com>
2008-02-15 08:08:10 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/fisshplate/trunk@162 c159f9c0-7f7c-4dca-964d-717b2124a8cb

---
+++ fisshplate/src/main/java/org/seasar/fisshplate/core/parser/AbstractElseParser.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.fisshplate.core.parser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.seasar.fisshplate.consts.FPConsts;
+import org.seasar.fisshplate.core.element.AbstractBlock;
+import org.seasar.fisshplate.core.element.IfBlock;
+import org.seasar.fisshplate.exception.FPParseException;
+import org.seasar.fisshplate.wrapper.CellWrapper;
+import org.seasar.fisshplate.wrapper.RowWrapper;
+
+
+/** 
+ * else if と else を解析するクラスのための抽象クラスです。
+ * @author rokugen
+ */
+public abstract class AbstractElseParser implements StatementParser {
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see org.seasar.fisshplate.core.parser.StatementParser#process(org.seasar.fisshplate.wrapper.CellWrapper, org.seasar.fisshplate.core.parser.FPParser)
+	 */
+	public boolean process(CellWrapper cell, FPParser parser)	throws FPParseException {
+		String value = cell.getStringValue();
+		Matcher mat = getPattern().matcher(value);
+		if(!mat.find()){
+			return false;
+		}
+		RowWrapper row = cell.getRow();
+		AbstractBlock parent = getParentIfBlock(row, parser);
+		String condition = null;
+		if(mat.groupCount() > 1){
+			condition = mat.group(1);
+		}
+		AbstractBlock block = createElement(condition);
+		((IfBlock) parent).setNextBlock(block);
+		parser.pushBlockToStack(block);
+		return true;
+	}
+
+	private AbstractBlock getParentIfBlock(RowWrapper row,FPParser parser) throws FPParseException {
+		if (parser.isBlockStackBlank()) {
+			throw new FPParseException(FPConsts.MESSAGE_ID_LACK_IF,
+					new Object[]{new Integer(row.getHSSFRow().getRowNum() + 1)});
+		}
+
+		AbstractBlock parent = parser.getLastElementFromStack();
+
+		if (!(parent instanceof IfBlock)) {
+			throw new FPParseException(FPConsts.MESSAGE_ID_LACK_IF,
+					new Object[]{new Integer(row.getHSSFRow().getRowNum() + 1)});
+		}
+		return parent;
+	}
+	
+	/**
+	 * 要素クラスを生成し戻します。
+	 * @param condition 条件式
+	 * @return 要素クラス
+	 */
+	protected abstract AbstractBlock createElement(String condition);
+	
+	/**
+	 * 解析対象か否かの判定に使う{@link Pattern}を戻します。
+	 * @return Patternクラス
+	 */
+	protected abstract Pattern getPattern();
+
+
+}
