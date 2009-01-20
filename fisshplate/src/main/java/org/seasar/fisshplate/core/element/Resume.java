b8de75bf2546560930f873a7fd0be39da84128b5
rokugensai <rokugensai@gmail.com>
2009-01-20 04:53:33 +0000

    Resumeでbreakしてなかったのを修正
    git-svn-id: https://www.seasar.org/svn/sandbox/fisshplate/trunk@233 c159f9c0-7f7c-4dca-964d-717b2124a8cb

--- fisshplate/src/main/java/org/seasar/fisshplate/core/element/Resume.java
+++ fisshplate/src/main/java/org/seasar/fisshplate/core/element/Resume.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -26,23 +26,22 @@ import org.seasar.fisshplate.exception.FPMergeException;
  * @author rokugen
  */
 public class Resume implements TemplateElement {
-	private String targetVar;
-	
-	public Resume(String targetVar){
-		this.targetVar = targetVar;
-	}
+    private String targetVar;
 
-	public void merge(FPContext context) throws FPMergeException {
-		Set susSet = context.getSuspendedSet();
-		for(Iterator itr = susSet.iterator();itr.hasNext();){
-			Suspend sus = (Suspend) itr.next();
-			String targetStr = sus.getEl().getOriginalCellValue();
-			if(targetStr.contains(targetVar)){
-				sus.resume(context);
-				susSet.remove(sus);
-			}
-		}
+    public Resume(String targetVar){
+        this.targetVar = targetVar;
+    }
 
-	}
-
+    public void merge(FPContext context) throws FPMergeException {
+        Set susSet = context.getSuspendedSet();
+        for(Iterator itr = susSet.iterator();itr.hasNext();){
+            Suspend sus = (Suspend) itr.next();
+            String targetStr = sus.getEl().getOriginalCellValue();
+            if(targetStr.contains(targetVar)){
+                sus.resume(context);
+                susSet.remove(sus);
+                break;
+            }
+        }
+    }
 }
