af88d175b8391d265e109f56c8e21f0eaa901d56
rokugensai <rokugensai@gmail.com>
2009-10-08 08:11:06 +0000

    java1.4なのにアノテーション使っちゃった・・・・ので直した
    git-svn-id: https://www.seasar.org/svn/sandbox/fisshplate/branches/0.1.5@266 c159f9c0-7f7c-4dca-964d-717b2124a8cb

--- maint-trunk/fisshplate/src/test/java/org/seasar/fisshplate/exception/FPExceptionTest.java
+++ maint-trunk/fisshplate/src/test/java/org/seasar/fisshplate/exception/FPExceptionTest.java
@@ -5,10 +5,8 @@ import java.util.ResourceBundle;
 import junit.framework.TestCase;
 
 public class FPExceptionTest extends TestCase {
-	@SuppressWarnings("serial")
 	public void testリソースのカスタマイズ(){
 		FPException ex = new FPException("00001"){
-			@Override
 			protected ResourceBundle getExceptionBundle(){
 				return ResourceBundle.getBundle("customMessages");
 			}
